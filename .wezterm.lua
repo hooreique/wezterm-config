b1e28cb0fe9dcf6b8a7883d58fa8bbedb1cefd72
송제영 <46372718+hooreique@users.noreply.github.com>
2024-02-01 09:30:10 +0900

    Add .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- use powershell
+config.default_prog = { 'C:/Users/hmgngv/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe' }
+config.color_scheme = 'Monokai Remastered'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 11.5
+config.initial_cols = 120
+config.initial_rows = 31
+-- disable ligature
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+-- and finally, return the configuration to wezterm
+return config
